d7c2271aec0921caebee4163cffc44d9622dc2cd
Oleg Seledets <35776736+olegseledets@users.noreply.github.com>
2021-07-31 15:10:52 +0000

    Tic-tac-toe

--- main.cpp
+++ main.cpp
@@ -1,92 +1,87 @@
-//Координаты начинаются с 1 и заканчиваются 3
-//По горизонтали Y, по вертикали X
 #include <iostream>
 
-void showField(char field[][3]){
-  std::cout << "-------" << std::endl;
-  for(int i = 0; i < 3; ++i){
-    for(int j = 0; j < 3; ++j){
-      std::cout << "|" << field[i][j];
-    }
-    std::cout << "|" << std::endl << "-------" << std::endl;
-  }
+
+void inputCoord (char field [][3], char ch)
+{
+	int x = 0, y = 0;
+	do {
+		std::cout << "\nХод " << ch << ". Введите координаты:\n X: ";
+		std::cin >> x;
+    std::cout << "Y: ";
+    std::cin >> y;
+	} while ((x < 0 || x > 2) || (y < 0 || y > 2) || (field[x][y] != '-'));
+
+	field[x][y] = ch;
 }
 
-bool winCheck(char field[][3]){
-  for(int i = 0; i < 3; ++i){
-    bool matchHorizont = false;
+void showField (char field [][3])
+{
+	for (int i = 0; i < 3; ++i) {
+		for (int j = 0; j < 3; ++j) {
+			std::cout << field[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+}
+
+bool isWin (char field[][3])
+{
+	for (int i = 0; i < 3; ++i)
+	{
+		bool matchHorizont = false;
 		bool matchVertical = false;
 		for (int j = 0; j < 2; ++j)
 		{
-			if ((field[i][j] == field[i][j+1]) && (field[i][j] != ' ') && matchHorizont) 
+			if ((field[i][j] == field[i][j+1]) && (field[i][j] != '-') && matchHorizont) 
 				return true;
 			else 
-				if ((field[i][j] == field[i][j+1]) && (field[i][j] != ' '))
+				if ((field[i][j] == field[i][j+1]) && (field[i][j] != '-'))
 					matchHorizont = true;
 			
-			if ((field[j][i] == field[j+1][i]) && (field[j][i] != ' ') && matchVertical) 
+			if ((field[j][i] == field[j+1][i]) && (field[j][i] != '-') && matchVertical) 
 				return true;
 			else 
-				if ((field[j][i] == field[j+1][i]) && (field[j][i] != ' '))
+				if ((field[j][i] == field[j+1][i]) && (field[j][i] != '-'))
 					matchVertical = true;
 		}
-  }
-  return false;
-}
+	}
 
-bool turnPlayer(int player, char symb, char field[][3]){
-  int x, y;
-  showField(field);
-  std::cout << "Ход игрока " << player << ": " <<std::endl;
-  std::cout << "X = ";
-  std::cin >> x;
-  std::cout << "Y = ";
-  std::cin >> y;
-  if (x >= 1 && x <= 3 && y >= 1 && y <= 3){
-    if(field[x-1][y-1] != ' '){
-      std::cout << "Это место занято!" << std::endl;
-      return false;
-    }
-    else{
-      field[x-1][y-1] = symb;
-      return true;
-    }
-  }
-  else{
-    std::cout << "Введены неверные координаты!" << std::endl;
-    return false;
-  }
-}
+	bool matchMainDiag = false;
+	bool matchSideDiag = false;
+	for (int i = 0; i < 2; ++i)
+	{
+		if ((field[i][i] == field[i+1][i+1]) && (field[i][i] != '-') && matchMainDiag)
+			return true;
+		else
+			if ((field[i][i] == field[i+1][i+1]) && (field[i][i] != '-'))
+				matchMainDiag = true;
 
-int main() {
-  // поле
-  char field[3][3] = {{' ',' ',' '},
-                      {' ',' ',' '},
-                      {' ',' ',' '}};
+		if ((field[i][2-i] == field[i+1][2-(i+1)]) && (field[i][2-i] != '-') && matchSideDiag)
+			return true;
+		else
+			if ((field[i][2-i] == field[i+1][2-(i+1)]) && (field[i][2-i] != '-'))
+				matchSideDiag = true;
+	}
 
-  int player = 1; // игрок
-  std::cout << "                 Координаты начинаются с 1 и заканчиваются 3 \n" <<
-               "                 По горизонтали Y, по вертикали X \n";
-  while(true){
-    if(winCheck(field)){
-      std::cout << "Победа!" << std::endl;
-      showField(field);
-      return 0;
-    }
-    if(player == 1){
-      while(!turnPlayer(player, 'X', field));
-      player = 2;
-    }
-    else{
-      while(!turnPlayer(player, 'O', field));
-      player = 1;
-    }
-  }
-
+	return false;
 }
 
 
+int main() 
+{
+	char playingField [3][3] = {'-', '-', '-',
+															'-', '-', '-',
+															'-', '-', '-'};
 
+	for (int i = 1; (i <= 9) && !(isWin(playingField)); ++i) {
+		inputCoord(playingField,((i % 2 == 0) ? 'X' : 'O'));
+		showField(playingField);
+		if (isWin (playingField))
+			std::cout << "\n" << ((i % 2 == 0) ? 'X' : 'O') << " Победа! <<<";
+	}
+	if (!isWin(playingField)) 
+		std::cout << "\nНичья\n";
+}
 /*
 Задача 2. Крестики-нолики
 
